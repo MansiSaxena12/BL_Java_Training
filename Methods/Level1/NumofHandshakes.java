37f6dcdc14455b292dd04d371422e4ec5621efe0
Mansi Saxena <120730390+MansiSaxena12@users.noreply.github.com>
2026-02-06 11:01:34 +0530

    Delete Methods directory

--- Methods/Level1/NumofHandshakes.java
+++
@@ -1,9 +0,0 @@
-package Methods.Level1;
-import java.util.*;
-public class NumofHandshakes {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
-        System.out.println((num*(num+1))/2);
-    }
-}
